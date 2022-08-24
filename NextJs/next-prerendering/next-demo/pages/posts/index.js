f1accb95c76f0b8c06595d04f0e0796276cc2956
Pierre LE BRIS <pierre.lebris@outlook.com>
2022-08-24 14:23:40 +0200

    Create index.js

---
+++ NextJs/next-prerendering/next-demo/pages/posts/index.js
@@ -0,0 +1,32 @@
+function PostList() {
+  return (
+    <>
+      <h1>List of Posts</h1>
+      {
+        posts.map(post => {
+          return (
+            <div key={post.id}>
+              <h2>
+                {post.id} {post.title}
+              </h2>
+              <hr/>
+            </div>
+          )
+        })
+      }
+    </>
+  )
+}
+
+export default PostList
+
+export async function getStaticProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await response.json()
+
+  return {
+    props: {
+      posts: data.slice(0,3)
+    }
+  }
+}
